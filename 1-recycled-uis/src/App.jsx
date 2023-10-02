c8eea9c43dd2326545d95a07af684188ac5a796e
LarsG <lars.gunnar@jobloop.no>
2023-10-02 11:24:58 +0200

    add basic component

--- 1-recycled-uis/src/App.jsx
+++ 1-recycled-uis/src/App.jsx
@@ -1,9 +1,14 @@
 import { capitalize } from './utils/string-formatting'
+import { Logo } from './components/Logo/Logo'
 
 function App() {
   return (
     <div className='flex flex-col min-h-screen'>
-      <header>
+      <header className='flex justify-between px-2 py-4'>
+        <a href='/' className='w-32'>
+          <Logo />
+        </a>
+
         <h1 className='text-center'>
           {capitalize(import.meta.env.VITE_APP_NAME)} - v{import.meta.env.VITE_APP_VERSION}
         </h1>
