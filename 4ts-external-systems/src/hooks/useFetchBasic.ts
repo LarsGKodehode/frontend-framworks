510e32c8d6897e21629b106b6080360498138bac
LarsG <lars.gunnar@jobloop.no>
2023-11-16 11:51:48 +0100

    add useFetch basic example

---
+++ 4ts-external-systems/src/hooks/useFetchBasic.ts
@@ -0,0 +1,30 @@
+import { useEffect, useState } from "react"
+
+/**
+ * The basic steps for making a fetch request and returning an unknown data type
+ * Does not include error handling.
+ * 
+ * @param url - The URL we want to send a GET request to
+ */
+export default function useFetchBasic(url: string | URL) {
+  const [data, setData] = useState<null | any>(null)
+
+  const isLoading = !data
+
+  useEffect(() => {
+    async function makeRequest() {
+      const response = await fetch(url)
+      const data = await response.json()
+      setData(data)
+    }
+
+    makeRequest()
+  }, [url])
+
+  return {
+    /** Will contain any date recieved from the server */
+    data,
+    /** Stateful value of the request status */
+    isLoading
+  }
+}
